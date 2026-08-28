59828c50ae2c511ebf5b95f18b07f20746a51ef6
HenedioNeto <netopedrosa13@gmail.com>
2026-08-28 14:54:48 -0300

    fazendo outro teste para rollback

--- application/src/main/java/com/devopslab/application/controller/ApplicationController.java
+++ application/src/main/java/com/devopslab/application/controller/ApplicationController.java
@@ -17,6 +17,10 @@ public class ApplicationController {
 
     public ApplicationController(ApplicationProperties properties) {
         this.properties = properties;
+
+        if ("production".equals(properties.environment())) {
+            throw new RuntimeException("TESTE DE ROLLBACK");
+        }
     }
 
     @GetMapping("/")
