9aa12e3ed0b3365003aa924d5458e1d2584933e8
HenedioNeto <netopedrosa13@gmail.com>
2026-08-28 14:37:14 -0300

    nao deu certo

--- application/src/main/java/com/devopslab/application/controller/ApplicationController.java
+++ application/src/main/java/com/devopslab/application/controller/ApplicationController.java
@@ -17,10 +17,6 @@ public class ApplicationController {
 
     public ApplicationController(ApplicationProperties properties) {
         this.properties = properties;
-
-        if (true) {
-            throw new RuntimeException("TESTE DE ROLLBACK");
-        }
     }
 
     @GetMapping("/")
